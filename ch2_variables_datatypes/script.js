a072dc6736ae6ab9164c6ff8658e0c329279973d
sumeya ibrahim <sumeya0017@gmail.com>
2025-08-25 21:56:00 +0300

    I have updated variables and datatypes

--- ch2_variables_datatypes/script.js
+++ ch2_variables_datatypes/script.js
@@ -1,13 +1,17 @@
-var firstName = "sumaya"
-var lastName = "Ibrahim"
-var Age = 24
-var isStudent = true
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// var firstName = "sumaya" //string
+// var lastName = "Ibrahim" //string
+// var Age = 24 // number
+// var isStudent = true //boolean
+// console.log(firstName);
+// console.log(lastName);
+// console.log(Age * Age);
+// console.log(isStudent);
 
-console.log(lastName);
-console.log(Age);
-console.log(isStudent);
+
+// var capitalCity = null
+// //city = "Nairobi"
+// //city ="London"
+// console.log(capitalCity);
+
+var theBestThingInTheWorld = 5
+console.log(typeof theBestThingInTheWorld);
